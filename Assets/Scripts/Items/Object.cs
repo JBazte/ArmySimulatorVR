d092f69990c2bf80e65df9155504b4313c255539
Nicolas DG <37745277+Supernico12@users.noreply.github.com>
2019-07-05 12:16:13 -0300

    Weapon Added
    - Added Weapons and Objects

---
+++ Assets/Scripts/Items/Object.cs
@@ -0,0 +1,113 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR.InteractionSystem;
+
+[RequireComponent(typeof(Throwable))]
+public class Object : MonoBehaviour
+{
+    Interactable interactable;
+
+    private Hand.AttachmentFlags attachmentFlags = Hand.defaultAttachmentFlags & (~Hand.AttachmentFlags.TurnOnKinematic)
+    & (~Hand.AttachmentFlags.DetachOthers) & (~Hand.AttachmentFlags.VelocityMovement) & (Hand.AttachmentFlags.TurnOffGravity);
+
+    void Start()
+    {
+        interactable = GetComponentInChildren<Interactable>();
+    }
+    //-------------------------------------------------
+    // Called when a Hand starts hovering over this object
+    //-------------------------------------------------
+    private void OnHandHoverBegin(Hand hand)
+    {
+
+    }
+
+
+    //-------------------------------------------------
+    // Called when a Hand stops hovering over this object
+    //-------------------------------------------------
+    private void OnHandHoverEnd(Hand hand)
+    {
+
+    }
+
+
+    //-------------------------------------------------
+    // Called every Update() while a Hand is hovering over this object
+    //-------------------------------------------------
+    private void HandHoverUpdate(Hand hand)
+    {
+        GrabTypes startingGrabType = hand.GetGrabStarting();
+        bool isGrabEnding = hand.IsGrabEnding(this.gameObject);
+
+        if (interactable.attachedToHand == null && startingGrabType != GrabTypes.None)
+        {
+
+            hand.HoverLock(interactable);
+
+            // Attach this object to the hand
+            hand.AttachObject(gameObject, startingGrabType, attachmentFlags);
+        }
+        else if (isGrabEnding)
+        {
+            // Detach this object from the hand
+            hand.DetachObject(gameObject);
+
+            // Call this to undo HoverLock
+            hand.HoverUnlock(interactable);
+
+
+        }
+    }
+
+
+    //-------------------------------------------------
+    // Called when this GameObject becomes attached to the hand
+    //-------------------------------------------------
+    private void OnAttachedToHand(Hand hand)
+    {
+
+    }
+
+
+
+    //-------------------------------------------------
+    // Called when this GameObject is detached from the hand
+    //-------------------------------------------------
+    private void OnDetachedFromHand(Hand hand)
+    {
+
+    }
+
+
+    //-------------------------------------------------
+    // Called every Update() while this GameObject is attached to the hand
+    //-------------------------------------------------
+    private void HandAttachedUpdate(Hand hand)
+    {
+
+    }
+
+    private bool lastHovering = false;
+    private void Update()
+    {
+
+    }
+
+
+    //-------------------------------------------------
+    // Called when this attached GameObject becomes the primary attached object
+    //-------------------------------------------------
+    private void OnHandFocusAcquired(Hand hand)
+    {
+    }
+
+
+    //-------------------------------------------------
+    // Called when another attached GameObject becomes the primary attached object
+    //-------------------------------------------------
+    private void OnHandFocusLost(Hand hand)
+    {
+    }
+}
--- Assets/Scripts/Items/Weapon.cs
+++ Assets/Scripts/Items/Weapon.cs
@@ -1,18 +1,78 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Valve.VR.InteractionSystem;
 
 public class Weapon : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField]
+    private Shot shotInstance;
+    [SerializeField]
+    private Transform bulletSpawnPosition;
+    [SerializeField]
+    private float bulletForce = 10f;
+    [Header("Stats")]
+    [SerializeField]
+    private float damage = 1f;
+    [SerializeField]
+    private float maxAmmo = 30f;
+    [SerializeField]
+    private float totalReloadTime = 3f;
+    [SerializeField]
+    private float attackSpeed = 1f;
+
+    private float currentAmmo;
+    private float lastAttack;
+    private bool isReloading;
+
+    [SerializeField]
+    LayerMask attackMask;
+
+    private void Start()
     {
-        
+        currentAmmo = maxAmmo;
+    }
+    private void Update()
+    {
+        lastAttack -= Time.deltaTime;
+    }
+    public void Shoot()
+    {
+        if (lastAttack < 0)
+        {
+            if (currentAmmo > 0)
+            {
+                lastAttack = 1 / attackSpeed;
+                Shot instance = Instantiate(shotInstance);
+                instance.transform.position = bulletSpawnPosition.position;
+                instance.GetComponent<Rigidbody>().AddForce(bulletSpawnPosition.forward * bulletForce, ForceMode.Impulse);
+                instance.SetShot(damage, attackMask);
+                currentAmmo--;
+            }
+            else
+            {
+                if (!isReloading)
+                {
+                    Reload();
+                }
+            }
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private IEnumerator Reload()
     {
-        
+        isReloading = true;
+        yield return new WaitForSeconds(totalReloadTime);
+        currentAmmo = maxAmmo;
+        isReloading = false;
+    }
+
+    private void HandAttachedUpdate(Hand hand)
+    {
+
+        if (Input.GetKey(KeyCode.K))
+        {
+            Shoot();
+        }
     }
 }
