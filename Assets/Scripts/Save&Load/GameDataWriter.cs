f78e91c98d7d976a9435fe6b7a832c8eb674d60a
Supernico12 <37745277+Supernico12@users.noreply.github.com>
2019-08-09 12:13:06 -0300

    Save & Load Added
    - Added PersistantObjects
    - Added DataReader
    - Added DataWriter
    - Added Data Storage

---
+++ Assets/Scripts/Save&Load/GameDataWriter.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.IO;
+public class GameDataWriter
+{
+    BinaryWriter writer;
+    public GameDataWriter(BinaryWriter writer)
+    {
+        this.writer = writer;
+    }
+
+    public void Write(Vector3 data)
+    {
+        writer.Write(data.x);
+        writer.Write(data.y);
+        writer.Write(data.z);
+    }
+
+    public void Write(Quaternion data)
+    {
+        writer.Write(data.x);
+        writer.Write(data.y);
+        writer.Write(data.z);
+        writer.Write(data.w);
+    }
+
+    public void Write(int data)
+    {
+        writer.Write(data);
+    }
+
+    public void Write(float data)
+    {
+        writer.Write(data);
+    }
+    public void Write(string data)
+    {
+        writer.Write(data);
+    }
+
+    public void Write(Random.State value)
+    {
+        writer.Write(JsonUtility.ToJson(value));
+    }
+
+}
---
+++ Assets/Scripts/Save&Load/PersistableObject.cs
@@ -0,0 +1,19 @@
+﻿
+using UnityEngine;
+public class PersistableObject : MonoBehaviour
+{
+
+    public virtual void Save(GameDataWriter writer)
+    {
+        writer.Write(transform.position);
+        writer.Write(transform.rotation);
+        writer.Write(transform.localScale);
+    }
+
+    public virtual void Load(GameDataReader reader)
+    {
+        transform.position = reader.ReadVector3();
+        transform.rotation = reader.ReadQuaternion();
+        transform.localScale = reader.ReadVector3();
+    }
+}
